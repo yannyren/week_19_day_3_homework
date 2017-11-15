5a2cbb4f127c663f068449c4f33002b0d95680b7
Yan Ren <yannyren@gmail.com>
2017-11-15 14:41:12 +0000

    mvp

--- app/src/main/java/codeclan/com/rockpaperscissors/controllers/RuleActivity.java
+++ app/src/main/java/codeclan/com/rockpaperscissors/controllers/RuleActivity.java
@@ -2,15 +2,22 @@ package codeclan.com.rockpaperscissors.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import codeclan.com.rockpaperscissors.R;
 
 public class RuleActivity extends AppCompatActivity {
+    private TextView rule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rule);
+        this.rule = findViewById(R.id.rules_box);
+        String rules = "Rock beats Scissors; \nScissors beats Paper; \nPaper beats Rock!";
+
+        rule.setText(rules);
+
     }
 
 
